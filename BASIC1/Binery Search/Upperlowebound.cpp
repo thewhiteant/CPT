a95785205dee00a8326c0dbd8f91d085b1360c50
White Ant <trafi227@gmail.com>
2023-03-21 01:11:10 +0600

    /

---
+++ BASIC1/Binery Search/Upperlowebound.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+int upperbound(vector<int> $test, int )
+
+int main()
+{
+
+
+
+    
+    return 0;
+}
--- BASIC1/Binery Search/bs1.cpp
+++ BASIC1/Binery Search/bs1.cpp
@@ -3,8 +3,34 @@ using namespace std;
 
 
 int main(){
+    int n;
+    cin>>n;
+    int arr[n+1];
+    for (int i = 0; i < n; i++)
+    {
+        cin>>arr[i];
+    }
+    
+    int find;
+    cin>>find;
+    int lo = 0; 
+    int hi = n-1;
+    int mid;
+    while(hi-lo>1){
+        mid = (hi+lo)/2;
+        if(arr[mid]<find){
+            lo = mid+1;
+        }
+        else {
+            hi = mid;
+        }
+    }
+
+    if(arr[lo]==find || arr[hi]==find) cout<<" pawa gese"<<endl;
+    else cout<<"NOpe"<<endl;
 
 
+    
 
 
     return 0;
